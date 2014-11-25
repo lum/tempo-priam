d0d25c6327cc69406208e641a65c51b663c0ab8e
Steve <steve.lum@gmail.com>
2014-11-25 12:04:01 -0800

    copying template to cassandra conf dir

--- recipes/schema.rb
+++ recipes/schema.rb
@@ -8,6 +8,12 @@ ruby_block "checking cassandra cli port" do
   end
 end
 
+template '#{node.cassandra[:home_dir]}/conf/cassandra-schema.txt' do
+  source "cassandra.schema.txt.erb"
+  mode 0644
+  owner "cassandra"
+end
+
 # create the schema if this is the first node of a cassandra ring, or if this is NOT part of a ring (i.e. uber)
 #if node['name'].include?('cassandra1') || !node['name'].include?('cassandra')
   execute "Trying to create Tempo schema..." do
@@ -17,6 +23,6 @@ end
         if [ $? -eq 0 ]; then
             exit 0
         fi
-        bin/cassandra-cli -h #{node[:ipaddress]} -B -f #{node.cassandra[:conf_dir]}/cassandra-schema.txt"
+        bin/cassandra-cli -h #{node[:ipaddress]} -B -f #{node.cassandra[:home_dir]}/conf/cassandra-schema.txt"
   end
 #end
