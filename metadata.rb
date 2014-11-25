a0d943ae2887c88eb6d8b4b77db39106564b25bb
Steve <steve.lum@gmail.com>
2014-11-25 10:44:46 -0800

    version bump to 0.2.0 of tempo-priam, also added dependency

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,12 @@ maintainer_email 'steve@tempo.ai'
 license          'All rights reserved'
 description      'Installs/Configures tempo-priam'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
+
+
+%w{ amazon ubuntu }.each do |os|
+  supports os
+end
+
+depends "parted"
+depends "cassandra-priam"
