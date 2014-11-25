942312adfdf54db80d83b580c34a97888167384a
Steve <steve.lum@gmail.com>
2014-11-25 11:24:45 -0800

    added schema recipe

---
+++ recipes/schema.rb
@@ -0,0 +1,22 @@
+class Chef::Resource::RubyBlock
+    include PortTest
+end
+
+ruby_block "checking cassandra cli port" do
+  block do
+    port_check?( node[:ipaddress], 9160, 10 )
+  end
+end
+
+# create the schema if this is the first node of a cassandra ring, or if this is NOT part of a ring (i.e. uber)
+if node['name'].include?('cassandra1') || !node['name'].include?('cassandra')
+  execute "Trying to create Tempo schema..." do
+    cwd     node.cassandra[:home_dir]
+    user    'root'
+    command "echo \"show keyspaces;\" | bin/cassandra-cli -h #{node.cassandra[:ipaddress]} | grep \"Keyspace: tempo:\"
+        if [ $? -eq 0 ]; then
+            exit 0
+        fi
+        bin/cassandra-cli -h #{node.cassandra[:ipaddress]} -B -f #{node.cassandra[:conf_dir]}/cassandra-schema.txt"
+  end
+end
