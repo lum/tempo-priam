50f903bbea04962d4e867303335fe88283dcedc4
Steve <steve.lum@gmail.com>
2014-12-16 14:21:30 -0800

    fix schema checking

--- recipes/schema.rb
+++ recipes/schema.rb
@@ -15,14 +15,14 @@ template "#{node.cassandra[:home_dir]}/conf/cassandra-schema.txt" do
 end
 
 # create the schema if this is the first node of a cassandra ring, or if this is NOT part of a ring (i.e. uber)
-#if node['name'].include?('cassandra1') || !node['name'].include?('cassandra')
+# #if node['name'].include?('cassandra1') || !node['name'].include?('cassandra')
   execute "Trying to create Tempo schema..." do
     cwd     node.cassandra[:home_dir]
     user    'root'
-    command "echo \"show keyspaces;\" | bin/cassandra-cli -h #{node[:ipaddress]} | grep \"Keyspace: tempo:\"
+    command "echo 'describe keyspaces' | /opt/cassandra/bin/cqlsh #{node[:ipaddress]} | grep tempo
         if [ $? -eq 0 ]; then
             exit 0
         fi
-        bin/cassandra-cli -h #{node[:ipaddress]} -B -f #{node.cassandra[:home_dir]}/conf/cassandra-schema.txt"
+    bin/cassandra-cli -h #{node[:ipaddress]} -B -f #{node.cassandra[:home_dir]}/conf/cassandra-schema.txt"
   end
-#end
+# #end
